7e58682e2736488e0264464b1c11c2977a47742c
NAGATA Minoru <nagata@lean-system.co.jp>
2023-05-17 17:05:27 +0900

    リファクタリング

--- main.js
+++ main.js
@@ -1,109 +1,120 @@
-import * as THREE from "three"; // Three.jsライブラリをインポートします。
+import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
 
 (async () => {
-  // シーン、カメラ、レンダラー、キューブの宣言
-  let scene, camera, renderer, cube;
-
-  let loadedModel, boundingBoxHelper;
+  let scene, camera, renderer, texturedCube, loadedModel, boundingBoxHelper;
 
   /**
-   * シーンの初期化
+   * シーンを初期化します。
+   * シーン、カメラ、レンダラー、テクスチャ付きキューブ、モデル、バウンディングボックスヘルパーを作成し、シーンに追加します。
    */
-  const init = async () => {
-    // シーンを作成します。シーンはオブジェクトや光源を格納するコンテナです。
+  const initializeScene = async () => {
     scene = new THREE.Scene();
-
-    // カメラを作成します。ここでは透視投影カメラを使用します。
-    // 最初の引数は視野角(FOV)、次の引数はアスペクト比、最後の2つの引数はニアクリップとファークリップの平面です。
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    // WebGLを使用したレンダラーを作成します。これによりシーンが描画されます。
-    renderer = new THREE.WebGLRenderer();
-
-    // レンダラーのサイズを現在のウィンドウのサイズに設定します。
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // renderer.setClearColor(0xefefef);
-
-    // レンダラーをDOMに追加します。これにより描画結果が表示されます。
+    camera = createCamera();
+    renderer = createRenderer();
     document.body.appendChild(renderer.domElement);
 
-    // キューブのジオメトリ（形状）を作成します。BoxGeometryは幅、高さ、奥行きを引数とします。
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    texturedCube = createTexturedCube();
+    scene.add(texturedCube);
 
-    // テクスチャローダーを使用してテクスチャをロードします。テクスチャは画像を3Dオブジェクトにマッピングするために使用されます。
-    let path = document.location.pathname;
-    path = path.substring(0, path.lastIndexOf("/") + 1);
-    let baseUrl = document.location.origin + path;
-    const texture = new THREE.TextureLoader().load(`${baseUrl}textures/wall.jpg`);
-
-    // マテリアルを作成します。マテリアルはオブジェクトの表面の見た目（色、光沢等）を定義します。
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-
-    // ジオメトリとマテリアルからメッシュ（実際に描画される3Dオブジェクト）を作成します。
-    cube = new THREE.Mesh(geometry, material);
-
-    cube.position.x = -1.1;
-
-    // メッシュをシーンに追加します。
-    scene.add(cube);
-
-    const loader = new GLTFLoader();
-    loadedModel = await new Promise((resolve) =>
-      loader.load("./scene.glb", (object) => resolve(object.scene), undefined, (error) => console.log(error))
-    );
-
-    loadedModel.position.x = 1.1;
+    loadedModel = await loadModel();
     scene.add(loadedModel);
 
     boundingBoxHelper = new THREE.BoxHelper(loadedModel, 0xffff00);
-
     scene.add(boundingBoxHelper);
 
-    // カメラをZ軸上に移動（キューブを適切に見るため）
     camera.position.z = 7;
   };
 
   /**
-   * ウィンドウがリサイズされた時のイベントハンドラ
+   * カメラを作成します。
+   * @returns {THREE.PerspectiveCamera} 作成されたカメラ。
+   */
+  const createCamera = () => {
+    return new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  };
+
+  /**
+   * レンダラーを作成します。
+   * @returns {THREE.WebGLRenderer} 作成されたレンダラー。
+   */
+  const createRenderer = () => {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    return renderer;
+  };
+
+  /**
+   * テクスチャ付きのキューブを作成します。
+   * @returns {THREE.Mesh} 作成されたテクスチャ付きキューブ。
+   */
+  const createTexturedCube = () => {
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const texture = new THREE.TextureLoader().load(`${getBaseUrl()}textures/wall.jpg`);
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.x = -1.1;
+    return cube;
+  };
+
+  /**
+   * ベースURLを取得します。
+   * @returns {string} ベースURL。
+   */
+  const getBaseUrl = () => {
+    let path = document.location.pathname;
+    path = path.substring(0, path.lastIndexOf("/") + 1);
+    return document.location.origin + path;
+  };
+
+  /**
+   * モデルをロードします。
+   * @returns {Promise<THREE.Group>} ロードされたモデル。
+   */
+  const loadModel = async () => {
+    const loader = new GLTFLoader();
+    const model = await new Promise((resolve) =>
+      loader.load("./scene.glb", (object) => resolve(object.scene), undefined, (error) => console.log(error))
+    );
+    model.position.x = 1.1;
+    return model;
+  };
+
+  /**
+   * ウィンドウがリサイズされたときに実行されます。
+   * カメラとレンダラーのサイズを調整します。
    */
   const handleResize = () => {
-    // ウィンドウサイズの変更に対応するためにカメラのアスペクト比を再設定します。
     camera.aspect = window.innerWidth / window.innerHeight;
-    // カメラの投影行列を更新します。これはカメラの設定が変更された後に必要です。
     camera.updateProjectionMatrix();
-    // レンダラーのサイズを新しいウィンドウのサイズに再設定します。
     renderer.setSize(window.innerWidth, window.innerHeight);
   };
 
   /**
-   * アニメーションループ
+   * アニメーションを実行します。
+   * レンダリングループを開始し、オブジェクトをアニメーションさせ、シーンをレンダリングします。
    */
   const animate = () => {
-    // requestAnimationFrameを使用してブラウザにアニメーションを行うことを要求します。次の描画の準備ができたらanimate関数を呼び出します。
     requestAnimationFrame(animate);
-
-    // キューブの回転値を少しずつ増やしてアニメーションを作ります。
-    cube.rotation.x -= 0.005;
-    cube.rotation.y += 0.005;
-
-    loadedModel.rotation.x += 0.01;
-    loadedModel.rotation.y += 0.01;
-    loadedModel.rotation.z += 0.01;
-
-    boundingBoxHelper.update();
-
-    // シーンとカメラを渡して描画を実行します。
+    animateObjects();
     renderer.render(scene, camera);
   };
 
-  // ウィンドウのリサイズイベントにイベントハンドラを設定します。
+  /**
+   * オブジェクトをアニメーションさせます。
+   * キューブとモデルを回転させ、バウンディングボックスヘルパーを更新します。
+   */
+  const animateObjects = () => {
+    texturedCube.rotation.x -= 0.005;
+    texturedCube.rotation.y += 0.005;
+    loadedModel.rotation.x += 0.01;
+    loadedModel.rotation.y += 0.01;
+    loadedModel.rotation.z += 0.01;
+    boundingBoxHelper.update();
+  };
+
   window.onresize = handleResize;
 
-  // 初期化関数を呼び出してシーンをセットアップします。
-  await init();
-
-  // アニメーションループを開始します。
+  await initializeScene();
   animate();
 })();
